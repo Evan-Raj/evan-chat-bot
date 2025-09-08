f349926acb664defa2851d16f2bd73ee4c3889db
Evan-Raj <fahim29250@gmail.com>
2025-09-08 11:05:52 +0600

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,34 +1,23 @@
 // utils/logger.js
 
-const chalk = require('chalk');
+import chalk from 'chalk';
 
-// A function to print log messages based on their type
 const logger = {
-    /**
-     * Prints a log message to the console with a specific format.
-     * @param {string} message The message to be logged.
-     * @param {string} type The type of the message (e.g., 'info', 'warn', 'error').
-     */
-    log: (message, type = 'info') => {
+    info: (message) => {
         const timestamp = new Date().toLocaleTimeString('bn-BD');
-        
-        switch (type) {
-            case 'info':
-                console.log(`[${chalk.hex('#00FFFF')('INFO')}] [${chalk.gray(timestamp)}] ${message}`);
-                break;
-            case 'warn':
-                console.log(`[${chalk.hex('#FFBF00')('WARN')}] [${chalk.gray(timestamp)}] ${chalk.yellow(message)}`);
-                break;
-            case 'error':
-                console.error(`[${chalk.hex('#FF0000')('ERROR')}] [${chalk.gray(timestamp)}] ${chalk.red(message)}`);
-                break;
-            case 'custom':
-                console.log(chalk.bold.hex('#E600FF')(`∞∞ EVAN RAJ » `) + chalk.hex('#F0FFF0')(`${message}`));
-                break;
-            default:
-                console.log(`[${chalk.gray('LOG')}] ${message}`);
-        }
+        console.log(`[${chalk.hex('#00FFFF')('INFO')}] [${chalk.gray(timestamp)}] ${message}`);
+    },
+    warn: (message) => {
+        const timestamp = new Date().toLocaleTimeString('bn-BD');
+        console.log(`[${chalk.hex('#FFBF00')('WARN')}] [${chalk.gray(timestamp)}] ${chalk.yellow(message)}`);
+    },
+    error: (message) => {
+        const timestamp = new Date().toLocaleTimeString('bn-BD');
+        console.error(`[${chalk.hex('#FF0000')('ERROR')}] [${chalk.gray(timestamp)}] ${chalk.red(message)}`);
+    },
+    custom: (message) => {
+        console.log(chalk.bold.hex('#E600FF')(`∞∞ EVAN RAJ » `) + chalk.hex('#F0FFF0')(`${message}`));
     }
 };
 
-module.exports = logger;
+export { logger };
